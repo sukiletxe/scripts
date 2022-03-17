c74fad8112156f3d29472f7a0a40024887f57942
Sukil Etxenike <sukiletxe@yahoo.es>
2022-03-17 11:38:28 +0100

    Updated notifyadded to Telethon v2

--- notifyadded.py
+++ notifyadded.py
@@ -1,11 +1,12 @@
 import logging
 logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s',
                     level=logging.WARNING)
-from telethon.sync import TelegramClient
+import asyncio
+
+from telethon import TelegramClient
 from telethon import events
 from telethon.tl.functions.messages import GetCommonChatsRequest, MarkDialogUnreadRequest
 from telethon.tl.types import UserEmpty
-import asyncio
 # This script requires Python 3 and Telethon, not sure about the minimum python version. I'm using 3.9.
 # Change this if you wish to store these values somewhere else. Default locations, format, and mini-guide:
 # Get the API key and API hash (for this script to be able to access Telegram in your behalf) by creating an application in my.telegram.org. Put them in a file called api.txt, each in one line, in that order.
@@ -23,7 +24,7 @@ async def wait_catch_up(client):
 
 
 async def handler(event):
-    me = await client.get_me()
+    me = await event.client.get_me()
     group = await event.get_chat()
     by = await event.get_added_by()
     if by is not None and me in event.users:
@@ -34,20 +35,20 @@ async def handler(event):
             nouser = "No username"
             username = f"[@{by.username}](@{by.username})" if by.username else nouser
             msg = f"{name} ({username}) has added you to {group.title}."
-        common_groups = await client(GetCommonChatsRequest(by, limit = 0, max_id = 0))
+        common_groups = await event.client(GetCommonChatsRequest(by, limit = 0, max_id = 0))
         common_groups = common_groups.chats
         common_groups.remove(group)
         if common_groups:
             common_titles = [x.title for x in common_groups]
             msg += "\nYou both have the following groups in common:\n"
             msg += '\n'.join(common_titles)
-        await client.send_message(me.id, msg)
-        await client(MarkDialogUnreadRequest(peer = me, unread = True))
+        await event.client.send_message(me.id, msg)
+        await event.client(MarkDialogUnreadRequest(peer = me, unread = True))
 
 async def main():
-    client.add_event_handler(handler, events.ChatAction)
-    await wait_catch_up(client)
-    await client.run_until_disconnected()
-with TelegramClient('tsession', api_id, api_hash) as client:
-    client.loop.run_until_complete(main())
+    async with await TelegramClient('tsession', api_id, api_hash).start() as client:
+        client.add_event_handler(handler, events.ChatAction)
+        await wait_catch_up(client)
+        await client.run_until_disconnected()
 
+asyncio.run(main())
